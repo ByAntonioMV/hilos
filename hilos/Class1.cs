6b0bc708a1241d06a0e4caeca17898522e402740
AntonioMV <li_antoniomv@unca.edu.mx>
2023-11-24 10:14:59 -0600

    Agregar archivos de proyecto.

---
+++ hilos/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace hilos
+{
+    public class Class1
+    {
+        
+        private String nombre;
+        
+        public Class1(String nombre)
+        {
+            this.nombre = nombre;
+        }
+        public Class1()
+        {
+            this.nombre = "";
+        }
+
+        public void SetNombre(String nombre)
+        {
+            this.nombre = nombre;
+        }
+        public String getNombre()
+        {
+            return this.nombre;
+        }
+        public void Run()
+        {
+
+            int i = 0;
+            while ( i <= 5)
+            {
+                
+                Console.WriteLine($"{nombre} Estan en el hilo alvvv.");
+
+                Thread.Sleep(1000);
+                i++;
+            }
+        }
+    }
+}
---
+++ hilos/Program.cs
@@ -0,0 +1,20 @@
+﻿using hilos;
+class Program
+{
+    static void Main()
+    {
+        Class1 jugador = new("Antonio");
+        Class1 jugador2 = new("David");
+
+        Thread jugadorThread = new Thread(jugador.Run);
+        jugadorThread.Start();
+
+        Thread jugador2Thread = new Thread(jugador2.Run);
+        jugador2Thread.Start();
+
+        //Console.WriteLine("Nombre del jugador: " + jugador.getNombre());
+
+
+
+    }
+}
